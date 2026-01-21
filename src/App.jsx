cef9777506839c63a8d2ee2d5fd2f3959cf41014
Robert Baravik <robert.baravik@sap.com>
2026-01-21 18:55:03 +0300

    feat: Add login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,160 @@
-import { useState, useEffect } from 'react'
-import Footer from './components/Footer'
-import Note from './components/Note'
-import Notification from './components/Notification'
-import noteService from './services/notes'
+import { useState, useEffect } from "react";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+import Notification from "./components/Notification";
+import noteService from "./services/notes";
+import loginService from "./services/login";
 
 const App = () => {
-  const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(true)
-  const [errorMessage, setErrorMessage] = useState(null)
+	const [notes, setNotes] = useState([]);
+	const [newNote, setNewNote] = useState("");
+	const [showAll, setShowAll] = useState(true);
+	const [errorMessage, setErrorMessage] = useState(null);
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    noteService.getAll().then(initialNotes => {
-      setNotes(initialNotes)
-    })
-  }, [])
+	useEffect(() => {
+		noteService.getAll().then((initialNotes) => {
+			setNotes(initialNotes);
+		});
+	}, []);
 
-  const addNote = event => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      important: Math.random() > 0.5
-    }
+	useEffect(() => {
+		const loggedUserJSON = window.localStorage.getItem("loggedNoteAppUser");
+		if (loggedUserJSON) {
+			const user = JSON.parse(loggedUserJSON);
+			setUser(user);
+			noteService.setToken(user.token);
+		}
+	}, []);
 
-    noteService.create(noteObject).then(returnedNote => {
-      setNotes(notes.concat(returnedNote))
-      setNewNote('')
-    })
-  }
+	const addNote = (event) => {
+		event.preventDefault();
+		const noteObject = {
+			content: newNote,
+			important: Math.random() > 0.5,
+		};
 
-  const toggleImportanceOf = id => {
-    const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: !note.important }
+		noteService.create(noteObject).then((returnedNote) => {
+			setNotes(notes.concat(returnedNote));
+			setNewNote("");
+		});
+	};
 
-    noteService
-      .update(id, changedNote)
-      .then(returnedNote => {
-        setNotes(notes.map(note => (note.id !== id ? note : returnedNote)))
-      })
-      .catch(error => {
-        setErrorMessage(
-          `Note '${note.content}' was already removed from server`
-        )
-        setTimeout(() => {
-          setErrorMessage(null)
-        }, 5000)
-        setNotes(notes.filter(n => n.id !== id))
-      })
-  }
+	const toggleImportanceOf = (id) => {
+		const note = notes.find((n) => n.id === id);
+		const changedNote = { ...note, important: !note.important };
 
-  const handleNoteChange = event => {
-    setNewNote(event.target.value)
-  }
+		noteService
+			.update(id, changedNote)
+			.then((returnedNote) => {
+				setNotes(
+					notes.map((note) => (note.id !== id ? note : returnedNote)),
+				);
+			})
+			.catch((error) => {
+				setErrorMessage(
+					`Note '${note.content}' was already removed from server`,
+				);
+				setTimeout(() => {
+					setErrorMessage(null);
+				}, 5000);
+				setNotes(notes.filter((n) => n.id !== id));
+			});
+	};
 
-  const notesToShow = showAll ? notes : notes.filter(note => note.important)
+	const handleNoteChange = (event) => {
+		setNewNote(event.target.value);
+	};
 
-  return (
-    <div>
-      <h1>Notes</h1>
-      <Notification message={errorMessage} />
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all'}
-        </button>
-      </div>
-      <ul>
-        {notesToShow.map(note => (
-          <Note
-            key={note.id}
-            note={note}
-            toggleImportance={() => toggleImportanceOf(note.id)}
-          />
-        ))}
-      </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit">save</button>
-      </form>
-      <Footer />
-    </div>
-  )
-}
+	const notesToShow = showAll
+		? notes
+		: notes.filter((note) => note.important);
 
-export default App
+	const handleLogin = async (event) => {
+		event.preventDefault();
+		try {
+			const user = await loginService.login({ username, password });
+			window.localStorage.setItem(
+				"loggedNoteAppUser",
+				JSON.stringify(user),
+			);
+			noteService.setToken(user.token);
+			setUser(user);
+			setUsername("");
+			setPassword("");
+		} catch {
+			setErrorMessage("wrong credentials");
+			setTimeout(() => {
+				setErrorMessage(null);
+			}, 5000);
+		}
+	};
+
+	const loginForm = () => (
+		<form onSubmit={handleLogin}>
+			<div>
+				<label>
+					username
+					<input
+						type="text"
+						value={username}
+						onChange={({ target }) => setUsername(target.value)}
+					/>
+				</label>
+			</div>
+			<div>
+				<label>
+					password
+					<input
+						type="password"
+						value={password}
+						onChange={({ target }) => setPassword(target.value)}
+					/>
+				</label>
+			</div>
+			<button type="submit">login</button>
+		</form>
+	);
+
+	const noteForm = () => (
+		<form onSubmit={addNote}>
+			<input value={newNote} onChange={handleNoteChange} />
+			<button type="submit">save</button>
+		</form>
+	);
+
+	return (
+		<div>
+			<h1>Notes</h1>
+			<Notification message={errorMessage} />
+
+			{!user && loginForm()}
+			{user && (
+				<div>
+					<p>{user.name} logged in</p>
+					{noteForm()}
+				</div>
+			)}
+
+			<div>
+				<button onClick={() => setShowAll(!showAll)}>
+					show {showAll ? "important" : "all"}
+				</button>
+			</div>
+			<ul>
+				{notesToShow.map((note) => (
+					<Note
+						key={note.id}
+						note={note}
+						toggleImportance={() => toggleImportanceOf(note.id)}
+					/>
+				))}
+			</ul>
+			<Footer />
+		</div>
+	);
+};
+
+export default App;
